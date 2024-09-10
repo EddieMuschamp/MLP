9214e2a8e19cfe6aefc3d08f152b35899c9d9b8f
Eddie <eddiemuschamp@gmail.com>
2024-09-10 17:01:57 +1000

    Moved Files

--- obj/Debug/netcoreapp3.1/MLP.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/MLP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4865dbbf4b82e00f94728b2b4d526a5208d562")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
